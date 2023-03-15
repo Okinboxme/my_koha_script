491ec037af4b7e16a4266ec1b794ba9e2d542cbe
okinboxme <Okinboxme@gmail.com>
2023-03-15 13:27:26 +0300

    Update install_koha.sh

--- install_koha.sh
+++ install_koha.sh
@@ -3,8 +3,12 @@
 printf "
 Powered by:
  ---
- (
- )
+   ____            _                  ___               _    _      _   _  __     _             ___           _        _ _           
+ / ___| _   _ ___| |_ ___ _ __ ___  / _ \ _ __   ___  | |  | |_ __| | | |/ /___ | |__   __ _  |_ _|_ __  ___| |_ __ _| | | ___ _ __ 
+ \___ \| | | / __| __/ _ \ '_ ` _ \| | | | '_ \ / _ \ | |  | __/ _` | | ' // _ \| '_ \ / _` |  | || '_ \/ __| __/ _` | | |/ _ \ '__|
+  ___) | |_| \__ \ ||  __/ | | | | | |_| | | | |  __/ | |__| || (_| | | . \ (_) | | | | (_| |  | || | | \__ \ || (_| | | |  __/ |   
+ |____/ \__, |___/\__\___|_| |_| |_|\___/|_| |_|\___| |_____\__\__,_| |_|\_\___/|_| |_|\__,_| |___|_| |_|___/\__\__,_|_|_|\___|_|   
+        |___/                                                                                                                       
 "
 
 
@@ -15,22 +19,22 @@ sudo apt-get update
 sudo apt-get update && apt upgrade -yq
 echo 'deb http://debian.koha-community.org/koha stable main' | sudo tee /etc/apt/sources.list.d/koha.list
 sudo apt-get update
-sudo dpkg-reconfigure locales;
+#sudo dpkg-reconfigure locales;
 #dependecy install
-sudo apt-get install -y libapache2-mpm-itk
+#sudo apt-get install -y libapache2-mpm-itk
 sudo apt-get install -y memcached
 sudo apt-get -y install mariadb-server
 sudo apt-get install -y koha-common
-sudo apt-get install -y multipath-tools
+#sudo apt-get install -y multipath-tools
 sudo systemctl restart memcached
 #koha basic config
 sudo a2enmod deflate
 sudo a2dismod mpm_event
-sudo a2enmod mpm_itk
+#sudo a2enmod mpm_itk
 sudo a2enmod rewrite
 sudo a2enmod cgi
 sudo systemctl restart apache2
-sudo koha-translate --install pt-PT
+#sudo koha-translate --install pt-PT
 sudo cp config/koha-sites.conf /etc/koha/
 sudo koha-create --create-db koha
 sudo rm /var/www/html/index.html
