ae115f21d7e32e172e0ce0d9454d50c707feddd0
okinboxme <Okinboxme@gmail.com>
2023-09-23 12:39:01 +0300

    Update auth.sh

--- auth.sh
+++ auth.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 extIP=$(wget -qO- https://ipecho.net/plain; echo);
-kohaPass=$(sudo koha-passwd koha);
+kohaPass=$(sudo koha-passwd mainlibrary);
 echo "";
 echo "/*********************************************************";
 echo "*******Access to http://"$extIP"            ***************";
 echo "**";
 echo "**";
 echo "**********************************************************";
-echo "USER: koha_koha"
+echo "USER: koha_mainlibrary"
 echo "PASS: "$kohaPass;
 echo ""
 echo "***********************/"
