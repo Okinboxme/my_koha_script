18b34b10f43f9f8367dd40de33624739260bb2e5
okinboxme <Okinboxme@gmail.com>
2023-09-23 13:11:03 +0300

    Update install_koha.sh

--- install_koha.sh
+++ install_koha.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-printf "
-Powered by:
- ---
-   ____            _                  ___               _    _      _   _  __     _             ___           _        _ _           
- / ___| _   _ ___| |_ ___ _ __ ___  / _ \ _ __   ___  | |  | |_ __| | | |/ /___ | |__   __ _  |_ _|_ __  ___| |_ __ _| | | ___ _ __ 
- \___ \| | | / __| __/ _ \ '_ ` _ \| | | | '_ \ / _ \ | |  | __/ _` | | ' // _ \| '_ \ / _` |  | || '_ \/ __| __/ _` | | |/ _ \ '__|
-  ___) | |_| \__ \ ||  __/ | | | | | |_| | | | |  __/ | |__| || (_| | | . \ (_) | | | | (_| |  | || | | \__ \ || (_| | | |  __/ |   
- |____/ \__, |___/\__\___|_| |_| |_|\___/|_| |_|\___| |_____\__\__,_| |_|\_\___/|_| |_|\__,_| |___|_| |_|___/\__\__,_|_|_|\___|_|   
-        |___/                                                                                                                       
-"
-
-
-#!/bin/bash
-
-apt-get update
-apt-get -y install sudo wget gnupg2
-wget -qO - https://debian.koha-community.org/koha/gpg.asc | gpg --dearmor -o /usr/share/keyrings/koha-keyring.gpg
-apt-get update -yq
+#Install script KOHA
+sudo wget -q -O- https://debian.koha-community.org/koha/gpg.asc | sudo apt-key add -
 echo 'deb http://debian.koha-community.org/koha oldstable main' | sudo tee /etc/apt/sources.list.d/koha.list
 sudo apt-get update
+sudo apt-get update && apt upgrade -yq
 
-sudo apt-get install koha-common
+#sudo apt-get update
 
-sudo apt-get install mariadb-server
+sudo apt-get -y install mariadb-server
+sudo apt-get install -y koha-common
 
-#Set up Apache
+#copy configuration from koha-sites
+sudo cp config/koha-sites.conf /etc/koha/
+sudo koha-create --create-db mainlibrary
+sudo rm /var/www/html/index.html
+sudo cp config/ports.conf /etc/apache2/
+
+#Setup plack
+sudo a2enmod headers proxy_http 
+#then:
+sudo koha-plack --enable mainlibrary
+sudo koha-plack --start mainlibrary
+sudo service apache2 restart
+
+#Apache config
 sudo a2enmod rewrite 
 sudo a2enmod cgi 
+
+
+#restart services
 sudo service apache2 restart
-#sudo koha-create --create-db libraryname
-sudo koha-create --create-db mainlibrary
-
-
-
 #Show login data
 sh auth.sh
+
